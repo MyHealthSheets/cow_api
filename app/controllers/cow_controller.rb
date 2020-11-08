7e00f0b15bd5f0ab7f97abca0bf32bbc010c523c
myhealthsheets <myhealthsheets@gmail.com>
2020-11-08 13:21:44 -0800

    First commit for Cowsaw API Demo

---
+++ app/controllers/cow_controller.rb
@@ -0,0 +1,11 @@
+class CowController < ApplicationController
+  def say
+    message      = params[:message]
+    cow          = params[:cow] || 'cow'
+    balloon_type = params[:balloon_type] || 'say'
+    face_type    = params[:face_type] || 'default'
+    @message = Cow
+      .new(cow: cow, face_type: face_type)
+      .say(message, balloon_type)
+  end
+end
